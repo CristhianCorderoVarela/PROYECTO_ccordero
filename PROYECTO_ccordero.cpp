6c86e285788ad5fda9e3266da6ffed816c9b2c7c
Cristhian Cordero Varela <cristhian.cordero.varela@est.una.ac.cr>
2024-08-25 14:16:00 -0600

    Project star

---
+++ PROYECTO_ccordero.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+
+
+void showMenu() {
+
+	cout << "   Menu de Opciones:" << endl;
+	cout << endl;
+	cout << "Archivo:" << endl;
+	cout << "   1. Acerca de" << endl;
+	cout << "   2. Salir" << endl;
+	cout << "Mantenimiento:" << endl;
+	cout << "   3. Peliculas" << endl;
+	cout << "   4. Salas" << endl;
+	cout << "   5. Horarios" << endl;
+	cout << "Reserva:" << endl;
+	cout << "   6. Peliculas" << endl;
+	cout << "Venta:" << endl;
+	cout << "   7. Peliculas" << endl;
+	cout << endl;
+	cout << "Seleccione una opcion: ";
+}
+
+
+int main()
+{
+	showMenu();
+	return 0; 
+}
+
